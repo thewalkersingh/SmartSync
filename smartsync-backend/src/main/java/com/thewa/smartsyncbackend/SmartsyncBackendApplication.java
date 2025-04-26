70ce559adc7debf7b2ed67635fb35ae4c9b07e52
Diwakar <the.walkersingh@hotmail.com>
2025-04-26 07:11:48 +0530

    CRUD started

---
+++ smartsync-backend/src/main/java/com/thewa/smartsyncbackend/SmartsyncBackendApplication.java
@@ -0,0 +1,10 @@
+package com.thewa.smartsyncbackend;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartsyncBackendApplication {
+   public static void main(String[] args) {
+	  SpringApplication.run(SmartsyncBackendApplication.class, args);
+   }
+}
---
+++ smartsync-backend/src/test/java/com/thewa/smartsyncbackend/SmartsyncBackendApplicationTests.java
@@ -0,0 +1,10 @@
+package com.thewa.smartsyncbackend;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartsyncBackendApplicationTests {
+   @Test
+   void contextLoads() {
+   }
+}
